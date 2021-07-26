db4b05ba725e25508894621cedede0f7e273a5b6
Tharun Kottam <62683279+TKottam@users.noreply.github.com>
2021-07-26 02:43:51 -0500

    commit

--- test/test_app.py
+++ test/test_app.py
@@ -1,5 +1,12 @@
 from app import FUN_root
+from flask.ext.testing import TestCase
 
 
-def test_index():
-    assert FUN_root() == "Flask Example"
+class MyTest(TestCase):
+
+    def create_app(self):
+        return app
+
+    def test_greeting(self):
+        self.app.get('/')
+        self.assert_template_used('index.html')
